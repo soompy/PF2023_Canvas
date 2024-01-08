88c91dfd3aeac236fd3e85c48dcfcf9151fc0199
soomin <sooper1137@gmail.com>
2024-01-08 16:19:20 +0900

    seo

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
+import path from 'path';
+
 export default {
   root: "",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+      "~": path.resolve(__dirname, "./"),
+    },
+  },
   build: {
     outDir: "../dist",
     emptyOutDir: true,
